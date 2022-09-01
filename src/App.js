eea33b5ec7a759882e75f712af586410ac9c5274
Luciano Giraudi <lucianoagiraudi@gmail.com>
2022-08-31 22:52:32 -0300

    after

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
+import React, { useState, useEffect } from 'react'
 import logo from './logo.svg';
 import './App.css';
-
+import Counter from './counter';
+import NavBar from './navBar';
+import Boton from './boton';
 function App() {
+  const stock = 10
+  const [items, setItems] = useState(0)
+  const [isRendered, setIsRendered] = useState(false)
+  const sumar = () => items < stock ? setItems(items + 1) : alert('Se alcanzo el maximo')
+  const restar = () => items > 0 ? setItems(items - 1) : alert('no se pueden introducir valores negativos')
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsRendered(true)
+    }, 100);
+  }, [])
+
+  useEffect(() => {
+    items !== 0 && console.log(`tengo : ${items} items`)
+  }, [items])
+
+  useEffect(() => {
+    console.log(!isRendered ? 'no renderizo' : 'renderizado')
+  }, [isRendered])
+
+
+
+  useEffect(() => {
+    console.log('me disparo una sola vez')
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar items={items} />
+      <Counter stock={stock} sumar={sumar} restar={restar} items={items} />
+      {items === 10 && <Boton />}
     </div>
   );
 }
